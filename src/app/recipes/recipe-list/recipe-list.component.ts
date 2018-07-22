00699ecd99e367537e67e8553087106179ed409f
hyperactive <hyperactive@hyperactives-Mac-mini-2.local>
2018-07-22 16:50:01 +0300

    another commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import {RecipeService} from '../recipe.service'
 import { Router, ActivatedRoute } from '../../../../node_modules/@angular/router';
+import { Subscription } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {  
+export class RecipeListComponent implements OnInit, OnDestroy {  
   recipes: Recipe[];
+  subscription: Subscription;
+
   constructor(private recipesService: RecipeService,
               private router:Router,
               private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.subscription = this.recipesService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      })
     this.recipes = this.recipesService.getRecipes();
   }
   onNewRecipe() {
     this.router.navigate(['new'],{relativeTo:this.route});
+  }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,12 @@
 import {Recipe} from './recipe.model';
 import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { Subject } from '../../../node_modules/rxjs';
 
 export class RecipeService{
-    
+    recipesChanged= new Subject<Recipe[]>();
+
     private recipes: Recipe[] = [
         new Recipe(
             'a test recipe', 
@@ -24,5 +27,20 @@ export class RecipeService{
     }
     getRecipe(index: number){
         return this.recipes[index];
+    }
+
+    addRecipe(recipe:Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index:number, newRecipe:Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index:number){
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
